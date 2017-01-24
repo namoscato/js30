cbbfd17a56ef46204c4a9c31f173ca245f337d7e
Nick Amoscato <nicholas@amoscato.com>
2017-01-23 21:38:18 -0500

    Cleanup canvas challenge

--- src/08/08.js
+++ src/08/08.js
@@ -1,122 +1,145 @@
 (() => {
-    const BOARD_SCALE = .5;
-    const MAGNETIC_RADIUS = 1000;
+    const BOARD_SCALE = 0.5;
+    const MAGNETIC_RADIUS = 4000;
 
-    const image = new Image();
-    const imagePadding = .05 * window.innerHeight;
-    const magneticRadisSquare = MAGNETIC_RADIUS * MAGNETIC_RADIUS;
+    const image = new window.Image();
+    const imagePadding = 0.05 * window.innerHeight;
+
+    function getRandomNumber(min, max) {
+        return (Math.random() * (max - min)) + min;
+    }
+
+    class Hair {
+
+        constructor(board) {
+            this.board = board;
+
+            this.x = getRandomNumber(0, this.board.getWidth());
+            this.y = getRandomNumber(0, this.board.getHeight());
+
+            this.draw();
+        }
+
+        move(mouseX, mouseY, deltaX, deltaY) {
+            const diffX = this.x - mouseX;
+            const diffY = this.y - mouseY;
+            const sum = (diffX * diffX) + (diffY * diffY);
+            const radius = getRandomNumber(MAGNETIC_RADIUS * 0.1, MAGNETIC_RADIUS);
+
+            let x = this.x;
+            let y = this.y;
+
+            if ((sum * sum) < (radius * radius)) {
+                x += deltaX + getRandomNumber(0, (mouseX - x) * 0.06);
+                y += deltaY + getRandomNumber(0, (mouseY - y) * 0.06);
+            }
+
+            if (this.board.isPositionAvailable(x, y)) {
+                this.x = x;
+                this.y = y;
+            }
+
+            this.draw();
+        }
+
+        draw() {
+            this.board.getContext().fillRect(this.x, this.y, 1, 1);
+        }
+    }
 
     class Board {
 
         constructor(hairCount) {
             const hairs = [];
 
-            this._canvas = document.getElementById('draw');
-            this._context = this._canvas.getContext('2d');
+            this.canvas = document.getElementById('draw');
+            this.context = this.canvas.getContext('2d');
+            this.contents = {};
 
-            this._canvas.height = window.innerHeight * BOARD_SCALE;
-            this._canvas.width = window.innerWidth * BOARD_SCALE;
-            this._canvas.style.height = `${window.innerHeight}px`;
-            this._canvas.style.width = `${window.innerWidth}px`;
+            this.canvas.height = window.innerHeight * BOARD_SCALE;
+            this.canvas.width = window.innerWidth * BOARD_SCALE;
+            this.canvas.style.height = `${window.innerHeight}px`;
+            this.canvas.style.width = `${window.innerWidth}px`;
 
             image.onload = () => {
                 image.style.height = `${window.innerHeight - (2 * imagePadding)}px`;
-                image.style.marginLeft = `${window.innerWidth / 2 - image.width / 2}px`;
+                image.style.marginLeft = `${(window.innerWidth / 2) - (image.width / 2)}px`;
                 image.style.marginTop = `${imagePadding}px`;
             };
 
             image.id = 'willy';
             image.src = 'willy.jpg';
 
-            this._canvas.parentNode.insertBefore(image, this._canvas);
+            this.canvas.parentNode.insertBefore(image, this.canvas);
 
-            for (let i = 0; i < hairCount; i++) {
+            for (let i = 0; i < hairCount; i += 1) {
                 hairs.push(new Hair(this));
             }
 
             document.onmousedown = (e) => {
-                this._mouseX = e.clientX * BOARD_SCALE;
-                this._mouseY = e.clientY * BOARD_SCALE;
-                this._isDragging = true;
-            }
+                this.mouseX = e.clientX * BOARD_SCALE;
+                this.mouseY = e.clientY * BOARD_SCALE;
+                this.draggingState = true;
+            };
 
             document.onmouseup = () => {
-                this._isDragging = false;
-            }
+                this.draggingState = false;
+            };
 
             document.onmousemove = (e) => {
-                if (!this._isDragging) {
+                if (!this.draggingState) {
                     return;
                 }
 
                 const mouseX = e.clientX * BOARD_SCALE;
                 const mouseY = e.clientY * BOARD_SCALE;
-                const deltaX = mouseX - this._mouseX;
-                const deltaY = mouseY - this._mouseY;
+                const deltaX = mouseX - this.mouseX;
+                const deltaY = mouseY - this.mouseY;
 
-                this._context.clearRect(0, 0, this._canvas.width, this._canvas.height);
+                this.contents = {};
+                this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
                 hairs.forEach((hair) => {
-                    hair.draw(mouseX, mouseY, deltaX, deltaY);
+                    hair.move(mouseX, mouseY, deltaX, deltaY);
                 });
 
-                this._mouseX = mouseX;
-                this._mouseY = mouseY;
+                this.mouseX = mouseX;
+                this.mouseY = mouseY;
             };
         }
 
         isDragging() {
-            return this._isDragging;
+            return this.draggingState;
+        }
+
+        isPositionAvailable(x, y) {
+            const xInteger = Math.floor(x);
+            const yInteger = Math.floor(y);
+
+            if (typeof this.contents[xInteger] === 'undefined') {
+                this.contents[xInteger] = {};
+            }
+
+            if (typeof this.contents[xInteger][yInteger] === 'undefined') {
+                this.contents[xInteger][yInteger] = true;
+                return true;
+            }
+
+            return false;
         }
 
         getWidth() {
-            return this._canvas.width;
+            return this.canvas.width;
         }
 
         getHeight() {
-            return this._canvas.height;
+            return this.canvas.height;
         }
 
         getContext() {
-            return this._context;
+            return this.context;
         }
     }
 
-    class Hair {
-
-        constructor(board, x, y) {
-            this._board = board;
-
-            this._x = this.getRandomNumber(0, this._board.getWidth());
-            this._y = this.getRandomNumber(0, this._board.getHeight());
-
-            this.draw();
-        }
-
-        draw(mouseX, mouseY, deltaX, deltaY) {
-            const diffX = this._x - mouseX;
-            const diffY = this._y - mouseY;
-            const sum = (diffX * diffX) + (diffY * diffY);
-            const radius = this.getRandomNumber(MAGNETIC_RADIUS * .1, MAGNETIC_RADIUS);
-
-            if ((sum * sum) < (radius * radius)) {
-                this._x += deltaX;
-                this._y += deltaY;
-
-                // if (sum > MAGNETIC_RADIUS) {
-                    this._x += this.getRandomNumber(0, (mouseX - this._x) * .1);
-                    this._y += this.getRandomNumber(0, (mouseY - this._y) * .1);
-                // }
-            }
-
-            this._board.getContext().fillRect(this._x, this._y, 2, 2);
-        }
-
-        getRandomNumber(min, max) {
-            return (Math.random() * (max - min)) + min;
-        }
-    }
-
-    const board = new Board(10000);
-
+    new Board(10000); // eslint-disable-line no-new
 })();
